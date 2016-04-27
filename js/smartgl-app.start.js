2a7647d00ff42104b43a33a1469d871a23a54bc2
Daniel Marinho <daniel_marinho@id.uff.br>
2016-04-28 00:25:15 +0200

    Collapsible sidebar menu implemented

--- js/smartgl-app.start.js
+++ js/smartgl-app.start.js
@@ -1,12 +1,17 @@
 angular.module('SmartGL',[
 
 ])
-controller('MainController', function($scope){
-  $scope.itens = [
-    {"id": 0, "name": "Default", "amount": "0g"}
+.controller('MainController', function($scope){
+  $scope.name = "Daniel Rossi Marinho";
+
+  $scope.items = [
+    {"id": 0, "name": "Default", "amount": "0g"},
+      {"id": 1, "name": "Bread", "amount": "550g"},
+        {"id": 2, "name": "Eggs", "amount": "2x"},
+          {"id": 3, "name": "Chocolate", "amount": "200g"}
   ];
 
   $scope.recipes = [
-    {"id": 0, "Name": "Default", "itens": ["Default"]}
+    {"id": 0, "Name": "Default", "items": [$scope.items[3],$scope.items[1]]}
   ];
 })
