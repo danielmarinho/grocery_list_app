3bbe55c9eaebe95d71cc987e230d2398204f2e98
Daniel Marinho <daniel_marinho@id.uff.br>
2016-04-28 12:15:07 +0200

    Menu active item highlighted

--- js/smartgl-app.start.js
+++ js/smartgl-app.start.js
@@ -4,6 +4,8 @@ angular.module('SmartGL',[
 .controller('MainController', function($scope){
   $scope.name = "Daniel Rossi Marinho";
 
+  $scope.active = "";
+
   $scope.items = [
     {"id": 0, "name": "Default", "amount": "0g"},
       {"id": 1, "name": "Bread", "amount": "550g"},
@@ -12,6 +14,12 @@ angular.module('SmartGL',[
   ];
 
   $scope.recipes = [
-    {"id": 0, "Name": "Default", "items": [$scope.items[3],$scope.items[1]]}
+    {"id": 0, "name": "Default", "items": [$scope.items[3],$scope.items[1]]},
+    {"id": 0, "name": "Kapsalon", "items": [$scope.items[0],$scope.items[2]]}
   ];
+
+function menu_selected(){
+return $scope.active = "active";
+}
+  $scope.menu_selected = menu_selected;
 })
